41a6426b62bdb00ee79f7d7dadd310be15f06d80
Gaurav-Krishna-Gaali <your_email_address@example.comcls>
2025-01-19 17:03:21 +0530

    added production url to CORS

--- api/main.py
+++ api/main.py
@@ -18,7 +18,7 @@ app = FastAPI()
 # CORS Middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000", "https://code-quest-rosy.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
